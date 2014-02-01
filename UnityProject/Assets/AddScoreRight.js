745dc98a8f7eb8ea516dc804cfdeacad61eb6f39
bartoszbart <skupien.bartosz@gmail.com>
2014-02-01 15:35:46 +0100

    gfgfg
    gfgf

---
+++ UnityProject/Assets/AddScoreRight.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	//function OnCollisionEnter(collision : Collision) {
+	//	if(collision.gameObject.tag=="Sphere_temp_test3best2"){ 
+	//		print("point");
+	 //	}
+	//}
+}
+
+function Update () {
+
+}
